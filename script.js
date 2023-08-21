73d059a79e598a963a22cf2f43d0d9cc14fdbecf
Iulian Gabriel <Burciuiuliangabriel18@gmail.com>
2023-08-21 17:12:03 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,75 @@
+const input = document.querySelector(".toDoInput");
+const inputButton = document.querySelector(".inputButton");
+const addToDoHere = document.querySelector(".dynamicallyAddToDo");
+let storeToDo = [];
+
+inputButton.addEventListener("click", function (e) {
+  addToList(e);
+});
+
+function addToList(e) {
+  e.preventDefault();
+  if (input.value === "") {
+    return;
+  }
+  storeToDo.push({
+    title: input.value,
+    id: storeToDo.length,
+    isChecked: false,
+  });
+  input.value = "";
+  displayToDo();
+}
+
+function displayToDo() {
+  addToDoHere.innerHTML = "";
+
+  storeToDo.forEach((el) => {
+    const todoItem = document.createElement("div");
+    todoItem.className = "todoItem";
+
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.checked = el.isChecked;
+
+    const todoText = document.createElement("span");
+    todoText.className = "todoText";
+    todoText.textContent = el.title;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "todoDeleteButton";
+    deleteButton.innerHTML = "&#10006";
+
+    if (el.isChecked) {
+      todoText.classList.add("checked");
+    }
+
+    todoItem.appendChild(checkBox);
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(deleteButton);
+
+    addToDoHere.appendChild(todoItem);
+
+    deleteButton.addEventListener("click", function (e) {
+      deleteToDo(el.id);
+    });
+
+    checkBox.addEventListener("click", function (e) {
+      checkToDo(el.id);
+    });
+  });
+}
+
+function deleteToDo(id) {
+  storeToDo = storeToDo.filter((el) => el.id !== id);
+  displayToDo();
+}
+
+function checkToDo(id) {
+  storeToDo.forEach((el) => {
+    if (el.id === id) {
+      el.isChecked = !el.isChecked;
+      displayToDo();
+    }
+  });
+}
